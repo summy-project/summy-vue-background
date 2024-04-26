ffa66236c5f9d573adc592908e270305bc55187f
Luke Na <narukeu@outlook.com>
2024-04-26 22:05:23 +0800

    无用文件删除

--- src/components/Table/index.ts
+++
@@ -1,9 +0,0 @@
-import CrudTable from "./CrudTable/index.vue";
-import SelectTable from "./SelectTable/index.vue";
-
-export default {
-  install(app: any) {
-    app.component("CrudTable", CrudTable);
-    app.component("SelectTable", SelectTable);
-  }
-};

a4891f7befecf8f54131af8cc663222602440166
Luke Na <narukeu@outlook.com>
2024-04-30 20:48:00 +0800

    修复 buildTree 构建数据错误的 bug

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -13,32 +13,27 @@ import type { SelectDataType } from "@/types";
 export function buildTree<
   T extends { id: string; children?: T[] },
   K extends keyof T
->(
-  menus: T[],
-  allMenus: T[],
-  parent?: T,
-  parentKey: K = "parentId" as K
-): T | undefined {
-  const result: T | undefined = parent ?? undefined;
+>(menus: T[], allMenus: T[], parent?: T, parentKey: K = "parentId" as K): T[] {
+  // 如果没有传入 parent 参数，创建一个新的顶层节点
+  const result: T[] = parent ? [parent] : [];
 
   // 遍历 menus 数组，构建树状结构
   menus.forEach((menu) => {
     // 如果存在父菜单，则将当前菜单添加到父菜单的 children 中
-    if (result) {
-      if (!result.children) {
-        result.children = [];
+    if (parent) {
+      if (!parent.children) {
+        parent.children = [];
       }
-      result.children.push(menu);
+      parent.children.push(menu);
+    } else {
+      // 如果没有父菜单，将当前菜单添加到结果中
+      result.push(menu);
     }
     // 寻找当前菜单项的所有子菜单项
     const childMenus = allMenus.filter((item) => item[parentKey] === menu.id);
     // 如果找到子菜单项，则递归构建子菜单
     if (childMenus.length > 0) {
-      const childResult = buildTree(childMenus, allMenus, menu, parentKey);
-      // 如果递归结果存在且当前菜单项尚未设置 children，则初始化 children 为递归结果
-      if (childResult && !menu.children) {
-        menu.children = [childResult];
-      }
+      buildTree(childMenus, allMenus, menu, parentKey);
     }
   });
 

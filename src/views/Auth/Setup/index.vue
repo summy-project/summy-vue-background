7f66e00884c59b539fc24d202831f1d7a5ab37f4
Luke Na <narukeu@outlook.com>
2024-05-02 14:31:50 +0800

    初始化的时候进行菜单设置

--- src/views/Auth/Setup/index.vue
+++ src/views/Auth/Setup/index.vue
@@ -47,14 +47,37 @@
 <script lang="ts" setup>
 import { reactive, ref, toRaw } from "vue";
 import { useRouter } from "vue-router";
+import { v4 as uuidv4 } from "uuid";
+
 import {
   MessagePlugin,
   type FormInstanceFunctions,
   type FormProps
 } from "tdesign-vue-next";
+import NotificationPlugin from "tdesign-vue-next/es/notification/plugin";
 
 import { http } from "@/utils/fetch";
 import { sha256 } from "@/utils/tools";
+
+interface SystemMenuType {
+  id: string;
+  createdTime: string;
+  updatedTime: string;
+  createdBy: string;
+  updatedBy: string;
+  remark: string;
+  name: string;
+  code: string;
+  pcIcon: string;
+  mobileIcon: string;
+  sort: number;
+  status: string;
+  parentId: string;
+  parentName: string;
+  pcRoute: string;
+  mobileRoute: string;
+  roleIds: string[];
+}
 
 const router = useRouter();
 
@@ -103,6 +126,141 @@ const pageData = reactive<Record<string, any>>({
     roleIds: ["visitor"],
     inviteCode: ""
   },
+  menus: [
+    {
+      id: "",
+      createdTime: "",
+      updatedTime: "",
+      createdBy: "root",
+      updatedBy: "",
+      remark: "",
+      name: "用户设置",
+      code: "user",
+      pcIcon: "user-safety",
+      mobileIcon: "",
+      sort: 1000,
+      status: "1",
+      parentId: "",
+      parentName: "",
+      pcRoute: "",
+      mobileRoute: "",
+      roleIds: ["user", "root", "admin"]
+    },
+    {
+      id: "",
+      createdTime: "",
+      updatedTime: "",
+      createdBy: "root",
+      updatedBy: "",
+      remark: "",
+      name: "用户列表",
+      code: "user_manage",
+      pcIcon: "",
+      mobileIcon: "",
+      sort: 1010,
+      status: "1",
+      parentId: "",
+      parentName: "user",
+      pcRoute: "/dashboard/user/manage",
+      mobileRoute: "",
+      roleIds: ["user", "root", "admin"]
+    },
+    {
+      id: "",
+      createdTime: "",
+      updatedTime: "",
+      createdBy: "root",
+      updatedBy: "",
+      remark: "",
+      name: "角色管理",
+      code: "user_roles",
+      pcIcon: "",
+      mobileIcon: "",
+      sort: 1020,
+      status: "1",
+      parentId: "",
+      parentName: "user",
+      pcRoute: "/dashboard/user/roles",
+      mobileRoute: "",
+      roleIds: ["user", "root", "admin"]
+    },
+    {
+      id: "",
+      createdTime: "",
+      updatedTime: "",
+      createdBy: "root",
+      updatedBy: "",
+      remark: "",
+      name: "邀请码管理",
+      code: "invite_code",
+      pcIcon: "",
+      mobileIcon: "",
+      sort: 1030,
+      status: "1",
+      parentId: "",
+      parentName: "user",
+      pcRoute: "/dashboard/user/inviteCode",
+      mobileRoute: "",
+      roleIds: ["user", "root", "admin"]
+    },
+    {
+      id: "",
+      createdTime: "",
+      updatedTime: "",
+      createdBy: "root",
+      updatedBy: "",
+      remark: "",
+      name: "系统相关",
+      code: "system",
+      pcIcon: "setting",
+      mobileIcon: "",
+      sort: 2000,
+      status: "1",
+      parentId: "",
+      parentName: "",
+      pcRoute: "",
+      mobileRoute: "",
+      roleIds: ["user", "root", "admin"]
+    },
+    {
+      id: "",
+      createdTime: "",
+      updatedTime: "",
+      createdBy: "root",
+      updatedBy: "",
+      remark: "",
+      name: "菜单管理",
+      code: "menu_manage",
+      pcIcon: "",
+      mobileIcon: "",
+      sort: 2010,
+      status: "1",
+      parentId: "",
+      parentName: "system",
+      pcRoute: "/dashboard/system/menu",
+      mobileRoute: "",
+      roleIds: ["user", "root", "admin"]
+    },
+    {
+      id: "",
+      createdTime: "",
+      updatedTime: "",
+      createdBy: "root",
+      updatedBy: "",
+      remark: "",
+      name: "字典管理",
+      code: "dict_manage",
+      pcIcon: "",
+      mobileIcon: "",
+      sort: 2020,
+      status: "1",
+      parentId: "",
+      parentName: "system",
+      pcRoute: "/dashboard/system/dict",
+      mobileRoute: "",
+      roleIds: ["user", "root", "admin"]
+    }
+  ],
   roles: [
     {
       id: "root",
@@ -176,6 +334,11 @@ const pageData = reactive<Record<string, any>>({
   }
 });
 
+const menuMainIds = {
+  user: uuidv4(),
+  system: uuidv4()
+};
+
 const initFormData = async () => {
   pageData.userData.createdTime = new Date().toISOString();
   pageData.visitorData.createdTime = new Date().toISOString();
@@ -204,9 +367,27 @@ const handleSubmit: FormProps["onSubmit"] = async () => {
       return item;
     });
 
+    postFormData.menus = postFormData.menus.map((item: SystemMenuType) => {
+      item.createdTime = new Date().toISOString();
+      if (item.code === "user" || item.code === "system") {
+        item.id = menuMainIds[item.code];
+      } else {
+        item.id = uuidv4();
+      }
+      if (item.parentName === "user") {
+        item.parentId = menuMainIds["user"];
+      } else if (item.parentName === "system") {
+        item.parentId = menuMainIds["system"];
+      } else {
+        item.parentId = "";
+      }
+      return item;
+    });
+
     const resultData = await http("POST", "/auth/setupProject", postFormData);
-    if (resultData.data.status === "success") {
-      MessagePlugin.success("初始化成功！");
+
+    if (resultData.status === "success") {
+      NotificationPlugin.success({ title: "提示", content: "初始化成功！" });
       router.push("/auth/login");
     }
   }
